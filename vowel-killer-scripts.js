1048a2b7e6296e6dd01c5f43dcbda9b012a6bb33
Anthony Gold and c.g. Acharjya <c.g.@>
2016-03-29 15:25:03 -0700

    added alert

--- vowel-killer-scripts.js
+++ vowel-killer-scripts.js
@@ -2,20 +2,22 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var unconverted = "A man, a plan, a canal, Panama";
+    var unconverted = $("#unconverted").val();
+    // todo: write input for var unconverted
     var vowels = ["a", "e", "i", "o", "u"];
     var phrase = unconverted.toLowerCase();
 
     for (var i = 0; i <vowels.length; i += 1) {
-      var loops = string.length;
+      var loops = phrase.length;
       for (p = 0; p < loops; p++) {
-          if (vowel[i] == string.charAt(p)) {
-            string = string.slice(0, p).concat(string.slice(p + 1, loops));
+          if (vowels[i] == phrase.charAt(p)) {
+            string = phrase.slice(0, p).concat(phrase.slice(p + 1, loops));
+            // todo: rewrite this to replace and not just delete vowels
             p -= 1;  // accounts for missing letter
             loops -= 1;  // string is now one character shorter
         }
       }
     }
-
+    alert(phrase);
   });
 });
