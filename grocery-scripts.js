8fb382348d04dcde91120d8f8b5d9e48077ee281
Anthony Gold and c.g. Acharjya <anthony@>
2016-03-29 13:25:13 -0700

    alphabetizes list using for instead of for-each

--- grocery-scripts.js
+++ grocery-scripts.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-  var inputs = ["item1", "item2", "item3", "item4", "item5"];
+    var inputs = ["item1", "item2", "item3", "item4", "item5"];
 
-  var groceries = inputs.map(function(input) {
+    var groceries = inputs.map(function(input) {
     return $("#" + input).val().toUpperCase();
-  });
-
-  groceries.sort();
-  groceries.forEach(function(grocery) {
-    if (grocery) {
-      $("#sorted").append("<li>" + grocery + "</li>")
-    }
     });
+
+    groceries.sort();
+    for (var inputs = 0; inputs <= 4; inputs += 1) {
+    $("#sorted").append("<li>" + groceries[inputs] + "</li>");
+    }
   });
 });
