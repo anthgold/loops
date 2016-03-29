12a7b0399e40d6aed86f1f9d2ff0a21e72c31292
Anthony Gold and c.g. Acharjya <c.g.@>
2016-03-29 10:02:13 -0700

    uses a for loop to count by 5 to 30

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-var countBy5 = {
-  for (var index =0; index >= 30; index += 5) {
-    alert(index);
-  };
-}
+// var countBy5 = {
+//   for (var index =0; index >= 30; index += 5) {
+//     total += countBy5 [index];
+//   console.log(index);
+//   };
+// }
+
+
+  for (var index =0; index <30; index +=5) {
+    var number = index +5;
+    console.log(number)
+  }
