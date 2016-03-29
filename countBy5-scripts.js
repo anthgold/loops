ebe689a3333cfc6969fc55b24d6cce6581fe7058
Anthony Gold and c.g. Acharjya <c.g.@>
2016-03-29 13:59:12 -0700

    undeletes html file

---
+++ countBy5-scripts.js
@@ -0,0 +1,12 @@
+// var countBy5 = {
+//   for (var index =0; index >= 30; index += 5) {
+//     total += countBy5 [index];
+//   console.log(index);
+//   };
+// }
+
+
+  for (var index =0; index <30; index +=5) {
+    var number = index +5;
+    console.log(number)
+  }
